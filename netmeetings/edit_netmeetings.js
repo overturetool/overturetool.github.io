5a80a60496d085479352fb510a1d69bc89fbfd95
Kenneth Lausdahl <kel@iha.dk>
2015-02-22 15:01:34 +0100

    changed to wait for page ready

--- netmeetings/edit_netmeetings.js
+++ netmeetings/edit_netmeetings.js
@@ -1,6 +1,12 @@
 function setEditButon() {
+ $(document).ready(function () {
+
+
         var node = document.getElementById("edit_page_div");
        	var n =location.pathname.split("/").slice(-1)+"";
 		var link ="https://github.com/overturetool/overturetool.github.io/tree/master/_netmeetings/"+n.substring(0,n.lastIndexOf("."))+".md";
     node.innerHTML  = "<a href=\""+ link+"\">Edit</a>" ;
-    };
+    });
+	
+	
+ };
