35ddf481ba2b1a236d89292d14418b43d35c1835
Kenneth Lausdahl <lausdahl@users.noreply.github.com>
2015-02-22 14:56:18 +0100

    Update edit_netmeetings.js

--- netmeetings/edit_netmeetings.js
+++ netmeetings/edit_netmeetings.js
@@ -3,4 +3,4 @@ function setEditButon() {
        	var n =location.pathname.split("/").slice(-1)+"";
 		var link ="https://github.com/overturetool/overturetool.github.io/tree/master/_netmeetings/"+n.substring(0,n.lastIndexOf("."))+".md";
     node.innerHTML  = "<a href=\""+ link+"\">Edit</a>" ;
-    });
+    };
